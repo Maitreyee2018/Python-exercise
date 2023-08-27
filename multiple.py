40bf9ec64ebecb7af87aaf13e700ef60547a61f4
Maitreyee2018 <42126649+Maitreyee2018@users.noreply.github.com>
2023-08-27 22:54:10 +0530

    Create multiple.py

---
+++ multiple.py
@@ -0,0 +1,14 @@
+This program takes two integers and returns True if the second one is a multiple of first one, returns False otherwise.
+
+def is_multiple(m,n):
+    if n%m==0:
+        return True
+    else:
+        return False
+
+
+m=int(input("Enter first number:"))
+n=int(input("Enter first number:"))
+print(is_multiple(m,n))
+ 
+   
